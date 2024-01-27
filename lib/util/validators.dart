0292a9e02a28792a947e657bd3c7d8e302565827
mahmoud-habashy <mr.habashy88@gmail.com>
2024-01-27 14:27:27 +0200

    create validators for email and password

---
+++ lib/util/validators.dart
@@ -0,0 +1,24 @@
+import 'package:coding_challenge/constants/strings.dart';
+
+RegExp _emailReq = RegExp(r"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$");
+RegExp _passwordReq = RegExp(r"^.{6,100}$");
+
+String? isValidEmail(String? email) {
+  email = email?.trim().toLowerCase();
+  if (email == null || email.isEmpty) {
+    return AppStrings.emailRequired;
+  } else if (!_emailReq.hasMatch(email)) {
+    return AppStrings.emailNotValid;
+  }
+  return null;
+}
+
+String? isValidPassword(String? password) {
+  password = password?.trim();
+  if (password == null || password.isEmpty) {
+    return AppStrings.passwordRequired;
+  } else if (!_passwordReq.hasMatch(password)) {
+    return AppStrings.passwordNotValid;
+  }
+  return null;
+}
