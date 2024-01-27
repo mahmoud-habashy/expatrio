e6d70119b7bda519ffba0f654392e0973bb42295
mahmoud-habashy <mr.habashy88@gmail.com>
2024-01-27 14:25:53 +0200

    create app config

---
+++ lib/config/app_config.dart
@@ -0,0 +1,9 @@
+class AppConfig {
+  static const String baseUrl = "https://dev-api.expatrio.com";
+
+  static const String loginUrl = "$baseUrl/auth/login";
+}
+
+class AppSecureStorageKey {
+  static const String loginUserDataKey = "user";
+}
