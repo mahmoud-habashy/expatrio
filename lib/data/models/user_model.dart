1164cb85fa7154f406a80963727b3dbd9677adc5
mahmoud-habashy <mr.habashy88@gmail.com>
2024-01-29 08:00:23 +0200

    edit user modal

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -1,111 +1,204 @@
 class UserModel {
-  late int userId;
-  String? accessToken;
-  DateTime? accessTokenExpiresAt;
-  late String userRole;
-  bool? xpm;
-  UserSubject? subject;
-  int? maxAgeSeconds;
+  late int id;
+  late String firstName;
+  late String lastName;
+  late String nationality;
+  late String profession;
+  late String emailAddress;
+  late String beginOfStudy;
+  late String phoneCountryCode;
+  late String phoneNumber;
+  late String dateOfBirth;
+  late String birthPlace;
+  late String birthCountry;
+  late Address germanAddress;
+  late Address foreignAddress;
+  late bool taxDataUpdatable;
+  late bool newTaxDataUpdatable;
+  late bool showAdditionalQuestions;
+  late bool showInterestMajor;
+  int? taxId;
+  String? title;
+  String? arrivalInGermany;
+  String universityName;
+  String universityCampus;
+  String notListedUniversity;
+  String specialization;
+  String notListedSpecialization;
+  String? visaStartDate;
+  String? germanLevel;
+  List<dynamic>? interestIndustry;
+  List<dynamic>? interestMajor;
+  List<dynamic>? preferredGermanCity;
+  List<dynamic>? preferredSocialMediaConnections;
+  List<dynamic>? interestTopics;
+  List<dynamic>? interestSocialIssues;
 
   UserModel({
-    required this.userId,
-    this.accessToken,
-    this.accessTokenExpiresAt,
-    required this.userRole,
-    required this.xpm,
-    this.subject,
-    this.maxAgeSeconds,
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.nationality,
+    required this.profession,
+    required this.emailAddress,
+    required this.beginOfStudy,
+    required this.phoneCountryCode,
+    required this.phoneNumber,
+    required this.dateOfBirth,
+    required this.birthPlace,
+    required this.birthCountry,
+    required this.germanAddress,
+    required this.foreignAddress,
+    required this.taxDataUpdatable,
+    required this.newTaxDataUpdatable,
+    required this.showAdditionalQuestions,
+    required this.showInterestMajor,
+    this.taxId,
+    this.title,
+    this.arrivalInGermany,
+    this.universityName = "",
+    this.universityCampus = "",
+    this.notListedUniversity = "",
+    this.specialization = "",
+    this.notListedSpecialization = "",
+    this.visaStartDate,
+    this.germanLevel,
+    this.interestIndustry,
+    this.interestMajor,
+    this.preferredGermanCity,
+    this.preferredSocialMediaConnections,
+    this.interestTopics,
+    this.interestSocialIssues,
   });
 
   Map<String, dynamic> toJson() {
     return {
-      'userId': userId,
-      'accessToken': accessToken,
-      'accessTokenExpiresAt': _serializeDateTime(accessTokenExpiresAt),
-      'userRole': userRole,
-      'xpm': xpm,
-      'subject': subject?.toJson(),
-      'maxAgeSeconds': maxAgeSeconds
+      'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
+      'nationality': nationality,
+      'profession': profession,
+      'emailAddress': emailAddress,
+      'beginOfStudy': beginOfStudy,
+      'phoneCountryCode': phoneCountryCode,
+      'phoneNumber': phoneNumber,
+      'dateOfBirth': dateOfBirth,
+      'birthPlace': birthPlace,
+      'birthCountry': birthCountry,
+      'germanAddress': germanAddress.toJson(),
+      'foreignAddress': foreignAddress.toJson(),
+      'taxDataUpdatable': taxDataUpdatable,
+      'newTaxDataUpdatable': newTaxDataUpdatable,
+      'showAdditionalQuestions': showAdditionalQuestions,
+      'showInterestMajor': showInterestMajor,
+      'taxId': taxId,
+      'title': title,
+      'arrivalInGermany': arrivalInGermany,
+      'universityName': universityName,
+      'universityCampus': universityCampus,
+      'notListedUniversity': notListedUniversity,
+      'specialization': specialization,
+      'notListedSpecialization': notListedSpecialization,
+      'visaStartDate': visaStartDate,
+      'germanLevel': germanLevel,
+      'interestIndustry': interestIndustry,
+      'interestMajor': interestMajor,
+      'preferredGermanCity': preferredGermanCity,
+      'preferredSocialMediaConnections': preferredSocialMediaConnections,
+      'interestTopics': interestTopics,
+      'interestSocialIssues': interestSocialIssues,
     };
   }
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
-      userId: json['userId'],
-      accessToken: json['accessToken'],
-      accessTokenExpiresAt: _parseServerDateTime(json['accessTokenExpiresAt']),
-      userRole: json['userRole'],
-      xpm: json['xpm'],
-      subject: UserSubject.fromJson(json['subject']),
-      maxAgeSeconds: json['maxAgeSeconds'],
+      id: json['id'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      nationality: json['nationality'],
+      profession: json['profession'],
+      emailAddress: json['emailAddress'],
+      beginOfStudy: json['beginOfStudy'],
+      phoneCountryCode: json['phoneCountryCode'],
+      phoneNumber: json['phoneNumber'],
+      dateOfBirth: json['dateOfBirth'],
+      birthPlace: json['birthPlace'],
+      birthCountry: json['birthCountry'],
+      germanAddress: Address.fromJson(json['germanAddress']),
+      foreignAddress: Address.fromJson(json['foreignAddress']),
+      taxDataUpdatable: json['taxDataUpdatable'],
+      newTaxDataUpdatable: json['newTaxDataUpdatable'],
+      showAdditionalQuestions: json['showAdditionalQuestions'],
+      showInterestMajor: json['showInterestMajor'],
+      taxId: json['taxId'],
+      title: json['title'],
+      arrivalInGermany: json['arrivalInGermany'],
+      universityName: json['universityName'],
+      universityCampus: json['universityCampus'],
+      notListedUniversity: json['notListedUniversity'],
+      specialization: json['specialization'],
+      notListedSpecialization: json['notListedSpecialization'],
+      visaStartDate: json['visaStartDate'],
+      germanLevel: json['germanLevel'],
+      interestIndustry: json['interestIndustry'] ?? [],
+      interestMajor: json['interestMajor'] ?? [],
+      preferredGermanCity: json['preferredGermanCity'] ?? [],
+      preferredSocialMediaConnections:
+          json['preferredSocialMediaConnections'] ?? [],
+      interestTopics: json['interestTopics'] ?? [],
+      interestSocialIssues: json['interestSocialIssues'] ?? [],
     );
-  }
-
-  static DateTime? _parseServerDateTime(String? fromServer) {
-    DateTime? result;
-    if (fromServer != null) {
-      result = DateTime.tryParse(fromServer)?.toLocal();
-    }
-    return result;
-  }
-
-  static String? _serializeDateTime(DateTime? dateTime) {
-    String? result;
-    if (dateTime != null) {
-      result = dateTime.toString();
-    }
-    return result;
   }
 }
 
-class UserSubject {
-  late int userId;
-  String? userUuid;
-  late String firstName;
-  late String lastName;
-  late String fullName;
-  late String email;
-  late String role;
-  late bool isAdmin;
-  List<dynamic>? consoleRoles;
+class Address {
+  late String streetName;
+  late String streetNumber;
+  late String postalCode;
+  late String city;
+  late String countryCode;
+  String coAddress;
+  String state;
+  String province;
+  String district;
 
-  UserSubject({
-    required this.userId,
-    this.userUuid,
-    required this.firstName,
-    required this.lastName,
-    required this.fullName,
-    required this.email,
-    required this.role,
-    required this.isAdmin,
-    this.consoleRoles,
+  Address({
+    required this.streetName,
+    required this.streetNumber,
+    required this.postalCode,
+    required this.city,
+    required this.countryCode,
+    this.coAddress = "",
+    this.state = "",
+    this.province = "",
+    this.district = "",
   });
 
   Map<String, dynamic> toJson() {
     return {
-      'userId': userId,
-      'userUuid': userUuid,
-      'firstName': firstName,
-      'lastName': lastName,
-      'fullName': fullName,
-      'email': email,
-      'role': role,
-      'isAdmin': isAdmin,
-      'consoleRoles': consoleRoles,
+      'streetName': streetName,
+      'streetNumber': streetNumber,
+      'postalCode': postalCode,
+      'city': city,
+      'countryCode': countryCode,
+      'coAddress': coAddress,
+      'state': state,
+      'province': province,
+      'district': district,
     };
   }
 
-  factory UserSubject.fromJson(Map<String, dynamic> json) {
-    return UserSubject(
-      userId: json['userId'],
-      userUuid: json['userUuid'],
-      firstName: json['firstName'],
-      lastName: json['lastName'],
-      fullName: json['fullName'],
-      email: json['email'],
-      role: json['role'],
-      isAdmin: json['isAdmin'] ?? false,
-      consoleRoles: json['consoleRoles'] ?? [],
+  factory Address.fromJson(Map<String, dynamic> json) {
+    return Address(
+      streetName: json['streetName'],
+      streetNumber: json['streetNumber'],
+      postalCode: json['postalCode'],
+      city: json['city'],
+      countryCode: json['countryCode'],
+      coAddress: json['coAddress'],
+      state: json['state'],
+      province: json['province'],
+      district: json['district'],
     );
   }
 }
