c1ebc71b74a8707e6e9d103c456d0f89f1af7bb3
mahmoud-habashy <mr.habashy88@gmail.com>
2024-01-30 04:13:33 +0200

    create tax data widget

---
+++ lib/presentation/widgets/tax_data.dart
@@ -0,0 +1,195 @@
+import 'package:coding_challenge/presentation/widgets/custom_checkbox.dart';
+import 'package:coding_challenge/presentation/widgets/action_result.dart';
+import 'package:coding_challenge/shared/app_assets.dart';
+import 'package:coding_challenge/shared/app_colors.dart';
+import 'package:coding_challenge/shared/app_constants.dart';
+import 'package:coding_challenge/data/models/tax_data_model.dart';
+import 'package:coding_challenge/logic/cubit/tax_data_cubit/tax_data_cubit.dart';
+import 'package:coding_challenge/logic/cubit/tax_item_drop_down_cubit/tax_item_drop_down_cubit.dart';
+import 'package:coding_challenge/presentation/screens/error_screen.dart';
+import 'package:coding_challenge/presentation/widgets/primary_button.dart';
+import 'package:coding_challenge/presentation/widgets/secondary_button.dart';
+import 'package:coding_challenge/presentation/widgets/tax_data_entry.dart';
+import 'package:coding_challenge/shared/app_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+/// Widget that supports displaying a column of [TaxEntryField].
+/// It has secondary button to add new tax data entry and checkbox for verifying the accuracy of the tax information
+/// before being able to submit it.
+/// it also has primary button for submit the tax data if the user checked the checkbox.
+///
+
+class TaxData extends StatefulWidget {
+  /// [userId] is required argument to call the API for fetching the tax data by user id.
+  ///
+  final int userId;
+  const TaxData({super.key, required this.userId});
+
+  @override
+  State<TaxData> createState() => _TaxDataState();
+}
+
+class _TaxDataState extends State<TaxData> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  int get _userId => widget.userId;
+
+  bool _isConfirmed = false;
+  bool _saveButtonClicked = false;
+  bool _updateTaxDataSuccess = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _getTaxData();
+  }
+
+  void _getTaxData() =>
+      context.read<TaxDataCubit>().getTaxDataByUserId(userId: _userId);
+
+  void _addNewTaxDataEntry() =>
+      context.read<TaxDataCubit>().addNewTaxData(Tax(country: '', id: ''));
+
+  void _removeTaxDataEntry(int index) =>
+      context.read<TaxDataCubit>().removeTaxData(index);
+
+  void _onFormSubmit() async {
+    setState(() => _saveButtonClicked = true);
+    if (_formKey.currentState!.validate() && _isConfirmed) {
+      await context.read<TaxDataCubit>().updateTaxDataByUserId(userId: _userId);
+      setState(() => _saveButtonClicked = false);
+    }
+  }
+
+  void _onUpdateTaxDataSuccess() {
+    setState(() => _updateTaxDataSuccess = true);
+    Future.delayed(
+        const Duration(milliseconds: AppConstants.animationDuration * 15), () {
+      if (mounted) {
+        Navigator.pop(context);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _updateTaxDataSuccess
+        ? const ActionResult(
+            lottieAsset: AppAssets.successAnimation,
+            title: AppStrings.taxDataUpdatedTitle,
+            subtitle: AppStrings.taxDataUpdatedSubtitle,
+          )
+        : BlocConsumer<TaxDataCubit, TaxDataState>(
+            listener: (context, state) {
+              if (state is TaxDataFetchSuccess) {
+                context.read<TaxItemDropDownCubit>().filterDropDownItems(
+                    state.taxDataModel.primaryTaxResidence,
+                    state.taxDataModel.secondaryTaxResidence);
+              } else if (state is TaxDataUpdateSuccess) {
+                _onUpdateTaxDataSuccess();
+              }
+            },
+            builder: (context, state) {
+              if (state is TaxDataFetchLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: AppColors.primary,
+                  ),
+                );
+              }
+              if (state is TaxDataFetchError) {
+                return ErrorScreen(
+                  title: AppStrings.taxDataModalErrorTitle,
+                  subtitle: state.errorMessage ??
+                      AppStrings.taxDataModalErrorSubtitle,
+                  buttonText: AppStrings.taxDataModalErrorBtnText,
+                  onRefresh: _getTaxData,
+                );
+              }
+              if (state is TaxDataFetchSuccess) {
+                return Form(
+                    key: _formKey,
+                    child: SingleChildScrollView(
+                      padding:
+                          const EdgeInsets.all(AppConstants.defaultPadding * 2),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(
+                                bottom: AppConstants.marginElement * 2),
+                            child: Text(AppStrings.taxDataModalTitle,
+                                style: Theme.of(context).textTheme.titleLarge),
+                          ),
+                          // Primary Tax Residence
+                          TaxEntryField(
+                            key: ObjectKey(
+                                state.taxDataModel.primaryTaxResidence),
+                            tax: state.taxDataModel.primaryTaxResidence,
+                          ),
+                          //
+                          // Secondary Tax Residences.
+                          for (int index = 0;
+                              index <
+                                  state.taxDataModel.secondaryTaxResidence
+                                      .length;
+                              index++)
+                            TaxEntryField(
+                                key: ObjectKey(state
+                                    .taxDataModel.secondaryTaxResidence[index]),
+                                tax: state
+                                    .taxDataModel.secondaryTaxResidence[index],
+                                index: index,
+                                onRemove: _removeTaxDataEntry),
+                          //
+                          Container(
+                            alignment: Alignment.bottomLeft,
+                            margin: const EdgeInsets.only(
+                                top: AppConstants.marginElement * 2,
+                                bottom: AppConstants.defaultMargin * 2),
+                            child: SecondaryButton(
+                              text: AppStrings.addMoreTaxEntryBtnText,
+                              textStyle: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(
+                                      color: AppColors.primary,
+                                      fontWeight: FontWeight.w700),
+                              icon: Icons.add,
+                              iconColor: AppColors.primary,
+                              iconSize: AppConstants.iconSize * 4,
+                              onPressed: _addNewTaxDataEntry,
+                            ),
+                          ),
+                          CustomCheckbox(
+                              text: AppStrings
+                                  .taxDataModalCheckBoxConfirmationText,
+                              checkBoxBorderColor:
+                                  (_saveButtonClicked && !_isConfirmed)
+                                      ? AppColors.accent
+                                      : AppColors.green,
+                              value: _isConfirmed,
+                              onChanged: (_) =>
+                                  setState(() => _isConfirmed = !_isConfirmed)),
+                          Container(
+                            margin: const EdgeInsets.only(
+                                top: AppConstants.defaultMargin * 2),
+                            width: AppConstants.containerElement * 16,
+                            child: PrimaryButton(
+                              text: AppStrings.saveTaxEntryBtnText,
+                              onPressed: () {
+                                _onFormSubmit();
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ));
+              }
+              return const SizedBox.shrink();
+            },
+          );
+  }
+}
---
+++ lib/presentation/widgets/tax_data_entry.dart
@@ -0,0 +1,208 @@
+import 'package:coding_challenge/presentation/widgets/custom_label.dart';
+import 'package:coding_challenge/shared/app_colors.dart';
+import 'package:coding_challenge/shared/app_constants.dart';
+import 'package:coding_challenge/data/models/item_dropdown.dart';
+import 'package:coding_challenge/data/models/tax_data_model.dart';
+import 'package:coding_challenge/logic/cubit/tax_data_cubit/tax_data_cubit.dart';
+import 'package:coding_challenge/logic/cubit/tax_item_drop_down_cubit/tax_item_drop_down_cubit.dart';
+import 'package:coding_challenge/presentation/widgets/secondary_button.dart';
+import 'package:coding_challenge/shared/app_strings.dart';
+import 'package:coding_challenge/util/validators.dart';
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+enum TaxEntryFieldType { country, id }
+
+/// A customized Widget for tax data entry it has two required input fields.
+/// The first one is country input allow user to select country the user will not be able to choose a previously selected country.
+/// The second one is Tax Number input allow user to enter his tax number.
+/// it has secondary button to allow the user to remove specific entry.
+/// 
+
+class TaxEntryField extends StatefulWidget {
+  /// The [tax] is required argument to access the Tax Data.
+  /// [index] is optional argument.
+  /// [onRemove] is optional argument. if this entry can be removed.
+  /// Both [index],[onRemove] != null => indicates that this Tax Entry is not Primary tax residence and can be removed.
+  ///
+  final Tax tax;
+  final int? index;
+  final Function(int)? onRemove;
+
+  const TaxEntryField(
+      {super.key, required this.tax, this.index, this.onRemove});
+
+  @override
+  State<TaxEntryField> createState() => _TaxEntryFieldState();
+}
+
+class _TaxEntryFieldState extends State<TaxEntryField> {
+  Tax get _taxDataEntry => widget.tax;
+  Function(int)? get _onRemove => widget.onRemove;
+  int? get _index => widget.index;
+  bool get _isPrimaryTaxResidence => _onRemove == null && _index == null;
+
+  final TextEditingController _taxController = TextEditingController();
+  ItemDropDown? _selectedDropDownItem;
+
+  @override
+  void initState() {
+    super.initState();
+    // Get the ItemDropDown by searching for country in the CountriesConstants.
+    _selectedDropDownItem = context
+        .read<TaxItemDropDownCubit>()
+        .getSelectedDropDownItem(country: _taxDataEntry.country);
+    // Fill the taxController with the tax number.
+    _taxController.text = _taxDataEntry.id;
+  }
+
+  @override
+  void dispose() {
+    // clean the controller.
+    _taxController.dispose();
+    super.dispose();
+  }
+
+  void _onEntryChanged(
+      {required TaxEntryFieldType taxEntryFieldType, required String value}) {
+    if (value.isNotEmpty) {
+      context.read<TaxDataCubit>().editTaxData(
+          index: _index, taxEntryFieldType: taxEntryFieldType, value: value);
+    }
+  }
+
+  Widget _buildLabelWidget({required String text}) {
+    return Container(
+      alignment: Alignment.topLeft,
+      padding: const EdgeInsets.only(left: AppConstants.paddingElement),
+      margin: const EdgeInsets.only(
+          bottom: AppConstants.marginElement,
+          top: AppConstants.marginElement * 5),
+      child: CustomLabel(
+        text: text,
+        textStyle: Theme.of(context).textTheme.labelMedium!,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        _buildLabelWidget(
+            text: _isPrimaryTaxResidence
+                ? AppStrings.taxDataModalPrimaryTitle
+                : AppStrings.taxDataModalSecondaryTitle),
+        BlocBuilder<TaxItemDropDownCubit, TaxItemDropDownState>(
+          builder: (context, state) {
+            return DropdownSearch<ItemDropDown>(
+              selectedItem: _selectedDropDownItem,
+              validator: AppValidators.isValidCountry,
+              onChanged: (value) {
+                if (value != null) {
+                  _onEntryChanged(
+                      taxEntryFieldType: TaxEntryFieldType.country,
+                      value: value.code);
+                }
+              },
+              items: context.watch<TaxItemDropDownCubit>().state.items,
+              popupProps: PopupProps.modalBottomSheet(
+                  showSearchBox: true,
+                  title: Container(
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      color: AppColors.primary,
+                      borderRadius: BorderRadius.only(
+                        topLeft:
+                            Radius.circular(AppConstants.radiusElement * 7),
+                        topRight:
+                            Radius.circular(AppConstants.radiusElement * 7),
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: AppConstants.paddingElement * 6),
+                    child: Text(AppStrings.taxDataModalCountryLabel,
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium!
+                            .copyWith(color: AppColors.white)),
+                  ),
+                  listViewProps: const ListViewProps(
+                      padding: EdgeInsets.only(
+                          bottom: AppConstants.defaultPadding * 2)),
+                  itemBuilder: (BuildContext context, ItemDropDown itemDropDown,
+                      bool isSelected) {
+                    return Padding(
+                      padding:
+                          const EdgeInsets.all(AppConstants.defaultPadding),
+                      child: Text(
+                        itemDropDown.label,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    );
+                  },
+                  searchFieldProps: TextFieldProps(
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                          hintText: AppStrings.taxDataModalCountryHintText,
+                          hintStyle: Theme.of(context).textTheme.bodyMedium,
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: AppConstants.paddingElement * 6,
+                              vertical: AppConstants.paddingElement * 2),
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(
+                                AppConstants.radiusElement * 3)),
+                          )))),
+            );
+          },
+        ),
+        _buildLabelWidget(text: AppStrings.taxDataModalTaxNumberLabel),
+        TextFormField(
+          autocorrect: false,
+          textInputAction: TextInputAction.done,
+          onChanged: (String value) {
+            _onEntryChanged(
+                taxEntryFieldType: TaxEntryFieldType.id, value: value.trim());
+          },
+          validator: AppValidators.isValidTaxId,
+          controller: _taxController,
+          scrollPadding: const EdgeInsets.symmetric(
+              vertical: AppConstants.defaultPadding * 3),
+          decoration: InputDecoration(
+              hintText: AppStrings.taxDataModalTaxNumberHintText,
+              hintStyle: Theme.of(context).textTheme.bodyMedium,
+              contentPadding: const EdgeInsets.symmetric(
+                  horizontal: AppConstants.paddingElement * 6,
+                  vertical: AppConstants.defaultPadding),
+              focusedBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(AppConstants.radiusElement)),
+                  borderSide: BorderSide(
+                    color: AppColors.primary,
+                  )),
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                    Radius.circular(AppConstants.radiusElement * 3)),
+              )),
+        ),
+        if (!_isPrimaryTaxResidence)
+          Container(
+            margin: const EdgeInsets.only(top: AppConstants.marginElement),
+            alignment: Alignment.bottomRight,
+            child: SecondaryButton(
+                text: AppStrings.removeTaxEntryBtnText,
+                textStyle: Theme.of(context).textTheme.bodySmall!.copyWith(
+                    color: AppColors.accent, fontWeight: FontWeight.w700),
+                icon: Icons.remove,
+                iconColor: AppColors.accent,
+                iconSize: AppConstants.iconSize * 3,
+                onPressed: () {
+                  _onRemove!(_index!);
+                }),
+          )
+      ],
+    );
+  }
+}
