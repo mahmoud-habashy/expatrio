8fdf360b243df80541a74910d6aba12eb6f22bca
mahmoud-habashy <mr.habashy88@gmail.com>
2024-01-30 04:03:53 +0200

    create login screen

---
+++ lib/presentation/screens/login_screen.dart
@@ -0,0 +1,215 @@
+import 'package:coding_challenge/config/app_config.dart';
+import 'package:coding_challenge/presentation/widgets/action_result.dart';
+import 'package:coding_challenge/shared/app_assets.dart';
+import 'package:coding_challenge/shared/app_colors.dart';
+import 'package:coding_challenge/shared/app_constants.dart';
+import 'package:coding_challenge/shared/app_strings.dart';
+import 'package:coding_challenge/data/models/auth_model.dart';
+import 'package:coding_challenge/logic/cubit/auth_cubit/auth_cubit.dart';
+import 'package:coding_challenge/presentation/widgets/bottom_modal.dart';
+import 'package:coding_challenge/presentation/widgets/chip_button.dart';
+import 'package:coding_challenge/presentation/widgets/login_entry_field.dart';
+import 'package:coding_challenge/presentation/widgets/primary_button.dart';
+import 'package:coding_challenge/util/app_router.dart';
+import 'package:coding_challenge/util/validators.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+/// Login Screen;
+/// This page could be opened:
+///  1. User opens the app for first time.
+///  2. The device has a invalid token.
+///  3. The device does not have token data.
+///
+
+class LoginScreen extends StatefulWidget {
+  /// [authModel] is optional argument, if you provide it email filed only will be filled with the user email.
+  /// if [authModel] is null email and password fields both will be empty.
+  ///
+  /// NOTE: For Testing in the initState method i override the email and password.
+  /// email: "tito+bs792@expatrio.com"
+  /// password: "nemampojma"
+  ///
+  final AuthModel? authModel;
+  const LoginScreen({super.key, this.authModel});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
+
+  final FocusNode _emailInputFocusNode = FocusNode();
+  final FocusNode _passwordInputFocusNode = FocusNode();
+
+  AuthModel? get _authModel => widget.authModel;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (_authModel != null && _authModel!.subject != null) {
+      _emailTextEditingController.text = _authModel!.subject!.email;
+    } else {
+      // TODO(Mahmoud) else block should be removed after testing.
+      // NOTE: This else block only for testing to add the default email and password.
+      _emailTextEditingController.text = "tito+bs792@expatrio.com";
+      _passwordTextEditingController.text = "nemampojma";
+      //
+    }
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controllers and focus nodes.
+    _emailTextEditingController.dispose();
+    _passwordTextEditingController.dispose();
+    _emailInputFocusNode.dispose();
+    _passwordInputFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _onFormSubmit() async {
+    if (_formKey.currentState!.validate()) {
+      String email = _emailTextEditingController.text;
+      String password = _passwordTextEditingController.text;
+      await context.read<AuthCubit>().login(email: email, password: password);
+    }
+  }
+
+  void _navigateToDashboard(AuthModel authModel) {
+    // Duration to give the bottom modal sheet time to dismiss.
+    Future.delayed(
+        const Duration(milliseconds: AppConstants.animationDuration * 2), () {
+      Navigator.of(context).pushReplacementNamed(AppRoutes.dashboardScreen,
+          arguments: authModel);
+    });
+  }
+
+  // Open the Expatrio website when user clicks on help chip button.
+  void _openWebsite() async {
+    final Uri uri = Uri.parse(AppConfig.fAQsWebsiteUrl);
+    if (await canLaunchUrl(uri)) {
+      launchUrl(uri);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocListener<AuthCubit, AuthState>(
+        listener: (context, state) {
+          if (state is AuthLoadingState) {
+            showSnackBarMessage(
+                context: context, message: AppStrings.loginSnackBarMessage);
+          } else if (state is AuthErrorState) {
+            openBottomSheet(
+                context: context,
+                content: ActionResult(
+                  lottieAsset: AppAssets.errorAnimation,
+                  repeatAnimation: true,
+                  title: AppStrings.loginFaildTitle,
+                  subtitle: state.errorMessage ?? AppStrings.loginFaildSubtitle,
+                  primaryBtnText: AppStrings.loginBottomModalsBtnText,
+                  onPrimaryBtnPressed: () => Navigator.of(context).pop(),
+                ));
+          } else if (state is AuthSuccessfulState) {
+            openBottomSheet(
+                context: context,
+                content: ActionResult(
+                  lottieAsset: AppAssets.successAnimation,
+                  repeatAnimation: false,
+                  title: AppStrings.loginSuccessTitle,
+                  subtitle: AppStrings.loginSuccessSubtitle,
+                  primaryBtnText: AppStrings.loginBottomModalsBtnText,
+                  onPrimaryBtnPressed: () => Navigator.of(context).pop(),
+                )).whenComplete(
+              () => _navigateToDashboard(state.authModel),
+            );
+          }
+        },
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Lottie.asset(AppAssets.loginBackgroundAnimation,
+                  repeat: true),
+            ),
+            Container(
+              color: AppColors.white.withOpacity(0.6),
+            ),
+            SingleChildScrollView(
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: AppConstants.defaultPadding * 2),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(
+                              top: AppConstants.defaultMargin,
+                              bottom: AppConstants.defaultMargin * 2),
+                          child: Image.asset(
+                            AppAssets.expatrioLogo,
+                            height: AppConstants.containerElement * 6,
+                            width: double.infinity,
+                          ),
+                        ),
+                        LoginEntryField(
+                          entryType: LoginEntryType.email,
+                          validator: AppValidators.isValidEmail,
+                          inputFocusNode: _emailInputFocusNode,
+                          nextInputFocusNode: _passwordInputFocusNode,
+                          textEditingController: _emailTextEditingController,
+                          hintText: AppStrings.emailEntryHintText,
+                        ),
+                        const SizedBox(
+                            height: AppConstants.containerElement * 2),
+                        LoginEntryField(
+                          entryType: LoginEntryType.password,
+                          validator: AppValidators.isValidPassword,
+                          inputFocusNode: _passwordInputFocusNode,
+                          textEditingController: _passwordTextEditingController,
+                          hintText: AppStrings.passwordEntryHintText,
+                          onFormSubmit: _onFormSubmit,
+                        ),
+                        const SizedBox(
+                            height: AppConstants.containerElement * 2),
+                        PrimaryButton(
+                          onPressed: _onFormSubmit,
+                          text: AppStrings.loginBtnText,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+                bottom: MediaQuery.of(context).padding.bottom,
+                child: Container(
+                  margin: const EdgeInsets.only(
+                      bottom: AppConstants.defaultMargin,
+                      left: AppConstants.defaultMargin),
+                  child: ChipButton(
+                      icon: Icons.question_mark_rounded,
+                      text: AppStrings.loginScreenHelpBtnText,
+                      onTap: _openWebsite),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/widgets/login_entry_field.dart
@@ -0,0 +1,164 @@
+import 'package:coding_challenge/presentation/widgets/custom_label.dart';
+import 'package:coding_challenge/shared/app_colors.dart';
+import 'package:coding_challenge/shared/app_constants.dart';
+import 'package:coding_challenge/shared/app_strings.dart';
+import 'package:flutter/material.dart';
+
+enum LoginEntryType { email, password }
+
+/// A customized Widget for login data entry.
+/// it can be used for inputting data for email or password depends on [LoginEntryType]
+/// case email:
+/// it has suffix button to clear input field and the textInputAction is next.
+/// case password:
+/// it has suffix button to show or hide the password and the textInputAction is done.
+///
+class LoginEntryField extends StatefulWidget {
+  /// The [entryType] is required argument to render password input field or email input field.
+  /// [textEditingController] is required argument for the input field.
+  /// [inputFocusNode] is required argument for the input field.
+  /// [hintText] is required argument for the input field hint text.
+  /// [validator] is required argument for input field validation.
+  /// [nextInputFocusNode] is optional argument. when user clicks on next in keyboard it request a focus node for next input field.
+  /// [onFormSubmit] is optional argument. when user clicks on done in keyboard it submit the form.(optional if the input field is email) AND (required if the input field is password).
+  ///
+  final LoginEntryType entryType;
+  final TextEditingController textEditingController;
+  final FocusNode inputFocusNode;
+  final String hintText;
+  final String? Function(String?) validator;
+  final FocusNode? nextInputFocusNode;
+  final VoidCallback? onFormSubmit;
+  const LoginEntryField(
+      {super.key,
+      required this.entryType,
+      required this.textEditingController,
+      required this.inputFocusNode,
+      required this.hintText,
+      required this.validator,
+      this.nextInputFocusNode,
+      this.onFormSubmit});
+
+  @override
+  State<LoginEntryField> createState() => _LoginEntryFieldState();
+}
+
+class _LoginEntryFieldState extends State<LoginEntryField> {
+  TextEditingController get _textEditingController =>
+      widget.textEditingController;
+  FocusNode get _inputFocusNode => widget.inputFocusNode;
+  LoginEntryType get _entryType => widget.entryType;
+  FocusNode? get _nextInputFocusNode => widget.nextInputFocusNode;
+  String get _hintText => widget.hintText;
+  VoidCallback? get _onFormSubmit => widget.onFormSubmit;
+
+  bool _passwordVisible = false;
+
+  @override
+  void initState() {
+    _textEditingController.addListener(_textEditingChange);
+    _inputFocusNode.addListener(_textEditingChange);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Remove the listeners.
+    _inputFocusNode.removeListener(() {});
+    _textEditingController.removeListener(() {});
+    super.dispose();
+  }
+
+  void _textEditingChange() => setState(() {});
+
+  void _passwordSuffixOnTap() =>
+      setState(() => _passwordVisible = !_passwordVisible);
+
+  void _emailSuffixOnTap() =>
+      Future.microtask(() => _textEditingController.clear());
+
+  void _onFieldSubmitted(String value) {
+    // case: email input field => request the next focus node.
+    if (_onFormSubmit == null) {
+      FocusScope.of(context).requestFocus(_nextInputFocusNode);
+      return;
+    }
+    // case: password input field => submit the login form.
+    _onFormSubmit!();
+  }
+
+  Widget _getSuffixIcon() {
+    IconData icon = Icons.close;
+    VoidCallback onPressed = _emailSuffixOnTap;
+    
+    if (_entryType == LoginEntryType.password) {
+      icon = _passwordVisible ? Icons.visibility_off : Icons.visibility;
+      onPressed = _passwordSuffixOnTap;
+    }
+    return IconButton(
+        onPressed: onPressed, icon: Icon(icon, color: AppColors.primary));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+            margin:
+                const EdgeInsets.only(bottom: AppConstants.marginElement * 2),
+            child: _getLabelWidget()),
+        TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          autocorrect: false,
+          obscureText:
+              _entryType == LoginEntryType.password ? !_passwordVisible : false,
+          textInputAction: _entryType == LoginEntryType.password
+              ? TextInputAction.done
+              : TextInputAction.next,
+          onTapOutside: (_) {
+            FocusScope.of(context).unfocus();
+          },
+          validator: widget.validator,
+          onFieldSubmitted: _onFieldSubmitted,
+          focusNode: _inputFocusNode,
+          controller: _textEditingController,
+          decoration: InputDecoration(
+              hintText: _hintText,
+              hintStyle: Theme.of(context).textTheme.bodyMedium,
+              contentPadding: const EdgeInsets.symmetric(
+                  horizontal: AppConstants.paddingElement * 6,
+                  vertical: AppConstants.defaultPadding),
+              suffixIcon: _textEditingController.text.isNotEmpty
+                  ? _getSuffixIcon()
+                  : null,
+              focusedBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(AppConstants.radiusElement * 2)),
+                  borderSide: BorderSide(
+                    color: AppColors.primary,
+                  )),
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                    Radius.circular(AppConstants.radiusElement * 2)),
+              )),
+        ),
+      ],
+    );
+  }
+
+  Widget _getLabelWidget() {
+    IconData icon = Icons.email_outlined;
+    String text = AppStrings.emailEntryLabelText;
+    if (_entryType == LoginEntryType.password) {
+      icon = Icons.lock_outline;
+      text = AppStrings.passwordEntryLabelText;
+    }
+    return CustomLabel(
+      text: text,
+      textStyle: Theme.of(context).textTheme.labelSmall!,
+      icon: icon,
+      iconSize: AppConstants.iconSize * 4,
+    );
+  }
+}
